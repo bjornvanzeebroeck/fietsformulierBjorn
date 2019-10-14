6f84ffc061bc299595673efdc76dc8a0558cde10
bjornvanzeebroeck <55879043+bjornvanzeebroeck@users.noreply.github.com>
2019-10-14 15:52:34 +0200

    initial setup

---
+++ script.js
@@ -0,0 +1,12 @@
+$ (document).ready(function(){
+    $(¨.fietsmaat > *").fadeOut();
+
+    $("[name='fiets']").on("change",function(){
+        let keuze = $("[name ='fiets']:checked").val();
+
+
+        if (keuze == "stadsfietsen-dames")
+    })
+
+
+});
