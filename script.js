07c79a52f2613f38c8c7d3a452876703807d4c2a
bjornvanzeebroeck <55879043+bjornvanzeebroeck@users.noreply.github.com>
2019-10-16 14:43:01 +0200

    woensdag update

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-$ (document).ready(function(){
-    $(¨.fietsmaat > *").fadeOut();
-
-    $("[name='fiets']").on("change",function(){
-        let keuze = $("[name ='fiets']:checked").val();
 
 
-        if (keuze == "stadsfietsen-dames") {
-            $([value="50"] , [value="50"], [value="50"])
-        }
-    })
+$(document).ready(function () {
+    
+    
+    $('[value="elektrischeFietsenHeren"]').on("change", function () {
+        $('label[for="fiets1"] , label[for="fiets2"] , label[for="fiets3"]').slideDown(1000);
+        $('label[for="fiets4"] , label[for="fiets5"] , label[for="fiets6"] , label[for="fiets7"] , label[for="fiets8"] , label[for="fiets9"] , label[for="fiets10"] , label[for="fiets11"] , label[for="fiets12"]').fadeOut(100);
+    });
+
+    $('[value="stadsfietsenHeren"]').on("change", function () {
+        $('label[for="fiets4"],label[for="fiets5"],label[for="fiets6"]').slideDown(1000);
+        $('label[for="fiets1"] , label[for="fiets2"] , label[for="fiets3"] , label[for="fiets7"] , label[for="fiets8"] , label[for="fiets9"] , label[for="fiets10"] , label[for="fiets11"] , label[for="fiets12"]').fadeOut(100);
+    });
+
+    $('[value="elektrischeFietsenDames"]').on("change", function () {
+        $('label[for="fiets7"],label[for="fiets8"],label[for="fiets9"]').slideDown(1000);
+        $('label[for="fiets1"] , label[for="fiets2"] , label[for="fiets3"] , label[for="fiets4"] , label[for="fiets5"] , label[for="fiets6"] , label[for="fiets10"] , label[for="fiets11"] , label[for="fiets12"]').fadeOut(100);
+    });
+
+    $('[value="stadsfietsenDames"]').on("change", function () {
+        $('label[for="fiets10"],label[for="fiets11"]').slideDown(1000);
+        $('label[for="fiets1"] , label[for="fiets2"] , label[for="fiets3"] , label[for="fiets4"] , label[for="fiets5"] , label[for="fiets6"] , label[for="fiets7"] , label[for="fiets8"] , label[for="fiets9"] , label[for="fiets12"]').fadeOut(100);
+    });
+
+    $('[value="vouwfietsen"]').on("change", function () {
+        $('label[for="fiets12"]').slideDown(1000);
+        $('label[for="fiets1"] , label[for="fiets2"] , label[for="fiets3"] , label[for="fiets4"] , label[for="fiets5"] , label[for="fiets6"] , label[for="fiets7"] , label[for="fiets8"] , label[for="fiets9"] , label[for="fiets10"] , label[for="fiets11"]').fadeOut(100);
+    });
 
 
 });
